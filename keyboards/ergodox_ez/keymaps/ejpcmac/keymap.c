873a1476ac7ad9d583b7a2820e827622f9fd2cc4
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2022-03-03 19:57:45 +0100

    feat(ejpcmac): reorganise the thumbs

--- keyboards/ergodox_ez/keymaps/ejpcmac/keymap.c
+++ keyboards/ergodox_ez/keymaps/ejpcmac/keymap.c
@@ -25,10 +25,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // |-------+-----+-----+-----+-----+-----|Enter|                              |Enter|-----+-----+-----+-----+-----+-------|
 // | Shift |  À  |  Y  |  X  |  .  |  K  |     |                              |     |  '  |  Q  |  G  |  H  |  F  |   W   |
 // `-------+-----+-----+-----+-----+-----------,-----------.      ,-----------,-----+-----+-----+-----+-----+-----+-------'
-//   |LCtrl| F/N | LAlt|AltGr|Super|           |PgUp |PgDwn|      |BÉPO | DEL |           |AltGr|LCtrl|Super|  Ç  |RCtrl|
+//   |LCtrl| F/N | LAlt|AltGr|Super|           |PgUp |PgDwn|      |BÉPO | DEL |           |LCtrl|AltGr|Super|  Ç  |RCtrl|
 //   `-----------------------------'     ,-----|-----|-----|      |-----+------+----.     `-----------------------------'
-//                                       |     |Shift|Inser|      | LFn |     |     |
-//                                       |Enter|Space|-----|      |-----|Shift|Space|
+//                                       |     |Shift|Inser|      | LFn |Shift|AltGr|
+//                                       |Space|Enter|-----|      |-----|Enter|Space|
 //                                       |     |     | ESC |      | F/N |     |     |
 //                                       `-----------------'      `-----------------'
 [BEPO] = LAYOUT_ergodox_pretty(
@@ -36,10 +36,10 @@ BP_DLR,     BP_DQUO,   BP_LDAQ,  BP_RDAQ,  BP_LPRN,  BP_RPRN,  KC_DEL,          
 KC_TAB,     BP_B,      BP_EACU,  BP_P,     BP_O,     BP_EGRV,  KC_BSPC,                          KC_BSPC,   BP_DCIR,  BP_V,     BP_D,     BP_L,     BP_J,     BP_Z,
 KC_ESC,     BP_A,      BP_U,     BP_I,     BP_E,     BP_COMM,                                               BP_C,     BP_T,     BP_S,     BP_R,     BP_N,     BP_M,
 KC_LSHIFT,  BP_AGRV,   BP_Y,     BP_X,     BP_DOT,   BP_K,     KC_ENTER,                         KC_ENTER,  BP_QUOT,  BP_Q,     BP_G,     BP_H,     BP_F,     BP_W,
-KC_LCTL,    MO(FNAV),  KC_LALT,  KC_ALGR,  KC_LGUI,                                                                   KC_ALGR,  KC_LCTL,  KC_RGUI,  BP_CCED,  KC_RCTL,
+KC_LCTL,    MO(FNAV),  KC_LALT,  KC_ALGR,  KC_LGUI,                                                                   KC_LCTL,  KC_ALGR,  KC_RGUI,  BP_CCED,  KC_RCTL,
                                                                KC_PGUP, KC_PGDOWN,    DF(BEPO),  KC_DEL,
                                                                         KC_INS,       DF(FNAV),
-                                              KC_ENTER, SFT_T(KC_SPC),  KC_ESC,       MO(FNAV),  KC_RSHIFT, KC_SPC),
+                                              KC_SPC, SFT_T(KC_ENTER),  KC_ESC,       MO(FNAV),  SFT_T(KC_ENTER), ALGR_T(KC_SPC)),
 
 // TODO: Add print.
 
