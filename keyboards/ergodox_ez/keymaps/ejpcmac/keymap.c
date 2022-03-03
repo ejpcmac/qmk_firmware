64e8eccb95018cf7fa9ed441fac8e87e02c83463
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2022-03-03 20:35:56 +0100

    feat(ejpcmac): swap AltGr & Shift on the right thumb

--- keyboards/ergodox_ez/keymaps/ejpcmac/keymap.c
+++ keyboards/ergodox_ez/keymaps/ejpcmac/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // `-------+-----+-----+-----+-----+-----------,-----------.      ,-----------,-----+-----+-----+-----+-----+-----+-------'
 //   |LCtrl| F/N | LAlt|AltGr|Super|           |PgUp |PgDwn|      |BÉPO | DEL |           |LCtrl|AltGr|Super|  Ç  |RCtrl|
 //   `-----------------------------'     ,-----|-----|-----|      |-----+------+----.     `-----------------------------'
-//                                       |     |Shift|Inser|      | LFn |Shift|AltGr|
+//                                       |     |Shift|Inser|      | LFn |AltGr|Shift|
 //                                       |Space|Enter|-----|      |-----|Enter|Space|
 //                                       |     |     | ESC |      | F/N |     |     |
 //                                       `-----------------'      `-----------------'
@@ -39,7 +39,7 @@ KC_LSHIFT,  BP_AGRV,   BP_Y,     BP_X,     BP_DOT,   BP_K,     KC_ENTER,        
 KC_LCTL,    MO(FNAV),  KC_LALT,  KC_ALGR,  KC_LGUI,                                                                   KC_LCTL,  KC_ALGR,  KC_RGUI,  BP_CCED,  KC_RCTL,
                                                                KC_PGUP, KC_PGDOWN,    DF(BEPO),  KC_DEL,
                                                                         KC_INS,       DF(FNAV),
-                                              KC_SPC, SFT_T(KC_ENTER),  KC_ESC,       MO(FNAV),  SFT_T(KC_ENTER), ALGR_T(KC_SPC)),
+                                              KC_SPC, SFT_T(KC_ENTER),  KC_ESC,       MO(FNAV),  ALGR_T(KC_ENTER), SFT_T(KC_SPC)),
 
 // TODO: Add print.
 
